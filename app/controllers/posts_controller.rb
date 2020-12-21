c8683afe328fa58811fce796bfa3811a09f272be
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-12-21 11:47:25 -0500

    Finished up commenting section

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,7 @@ class PostsController < ApplicationController
 
   def create
     @post = current_user.posts.build(post_params)
+    @post.comments&.first&.user = current_user
 
     if @post.save
       redirect_to posts_path, notice: "Created Post"
@@ -30,7 +31,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:image)
+    params.require(:post).permit(:image, comments_attributes: [:body])
   end
 
 end
