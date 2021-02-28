f2199643d4466d0c8dde45510063c5a52bc05cee
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-02-28 08:30:49 -0500

    Write up following

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  resources :followers, only: [:create, :destroy], param: :user_id
+
   resources :posts do
     resource :likes, only: [:create, :destroy], module: :posts
     resources :comments, only: [:show, :create, :new, :destroy], module: :posts
